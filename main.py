9395578ed5052a92a1fa45a0cf5caf1de3af054a
kiyoung.you <miroirs01@gmail.com>
2023-10-23 17:01:18 +0900

    implemented memory

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
 
 if "user_prompt_history" not in st.session_state:
     st.session_state["user_prompt_history"] = []
-if "chat_answers_history" not in st.session_state:
+if "chat_answer_history" not in st.session_state:
     st.session_state["chat_answer_history"] = []
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
 
 def create_source_string(source_urls: Set[str]) -> str:
@@ -27,15 +29,18 @@ def create_source_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response.."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_source_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_source_string(sources)}"
         )
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answer_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answer_history"]:
     for generated_response, user_query in zip(
